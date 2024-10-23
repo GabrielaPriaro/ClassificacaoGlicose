a0c6e538da470cb0133bc250c5abe1e9ef639448
Gabriela <gabriela.priaro@gmail.com>
2024-10-23 15:55:24 +0100

    Implementaçao para classificar valor de glicose.

--- ClassificacaoGlicose/Program.cs
+++ ClassificacaoGlicose/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace ClassificacaoGlicose
+﻿using System.Globalization;
+
+namespace ClassificacaoGlicose
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double valorGlicose;
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Console.Write("Digite o valor da glicose: ");
+            valorGlicose = double.Parse(Console.ReadLine(), CI);
+
+            Console.Write("Classificacao: ");
+            if (valorGlicose <= 100)
+            {
+                Console.WriteLine("normal");
+            }
+            else if (valorGlicose <= 140)
+            {
+                Console.WriteLine("elevado");
+            }
+            else
+            {
+                Console.WriteLine("diabetes");
+            }
         }
     }
 }
